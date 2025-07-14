e2cae43cf787740a98fe8ee5db00ce7abffeaa5f
Svet <70716489+ikyman@users.noreply.github.com>
2025-07-14 19:48:34 -0400

    TakeDirectory
    I must have missed the Take.ts when committing and pushing "Takes now store their folder name".
    
    How I missed that, I don't know. In any case, unless you like errors, you're also going to need this update

--- src/common/project/Take.ts
+++ src/common/project/Take.ts
@@ -1,10 +1,11 @@
-import { FrameCount, FrameRate, IsoDateTimeString, TakeId } from "../Flavors";
+import { FrameCount, FrameRate, IsoDateTimeString, TakeDirectory, TakeId } from "../Flavors";
 import { Track } from "./Track";
 
 export interface Take {
   id: TakeId;
   shotNumber: number;
   takeNumber: number;
+  takeDirectory: TakeDirectory;
   frameRate: FrameRate;
   holdFrames: FrameCount;
   frameTrack: Track;
